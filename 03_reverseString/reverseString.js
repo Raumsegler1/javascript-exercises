bc981a6f58252b09b4f489e2c4203c68ad3bff4a
Raumsegler1 <113139832+Raumsegler1@users.noreply.github.com>
2024-08-21 23:44:31 +0200

    Complete task 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    stringArray = string.split("");
+    reverseStringArray = stringArray.reverse();
+    reversedString = reverseStringArray.join("");
+    return reversedString;
 };
 
 // Do not edit below this line
