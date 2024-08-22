a69f8f1b466ba398b7cd3ab801662c6478349a8d
Raumsegler1 <113139832+Raumsegler1@users.noreply.github.com>
2024-08-22 02:18:34 +0200

    Complete task 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(numStart, numEnd) {
+    let total = 0;
+    if (numStart < 0 || numEnd < 0 || !Number.isInteger(numStart) || !Number.isInteger(numEnd)) {
+        return "ERROR"
+    }
+    for (let i = Math.min(numStart, numEnd); i <= Math.max(numStart, numEnd); i++) {
+        total += i;
+        console.log(total)
+    }
+    return total
 };
 
 // Do not edit below this line
