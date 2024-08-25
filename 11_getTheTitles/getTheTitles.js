f252220c2661c4dbedc1ee7056df40fbcf894997
Raumsegler1 <113139832+Raumsegler1@users.noreply.github.com>
2024-08-25 15:14:18 +0200

    Complete task 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let titles = [];
+    arr.map(key => titles.push(key.title));
+    return titles
 };
 
 // Do not edit below this line
