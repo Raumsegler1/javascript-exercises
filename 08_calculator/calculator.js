6da3b5f53f7c86d65ca8f84e9493890ef7ee884f
Raumsegler1 <113139832+Raumsegler1@users.noreply.github.com>
2024-08-25 14:16:23 +0200

    Complete task 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return Number(num1) + Number(num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return Number(num1) - Number(num2);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((last, current) => last * current, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Number(num1) ** Number(num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	let factorialSum = 1;
+  for (let i = num1; i > 1; i--) {
+    factorialSum *= i;
+  }
+  return factorialSum;
 };
 
 // Do not edit below this line
