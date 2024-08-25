460026270695a826b827e145c29c3248d08d889d
Raumsegler1 <113139832+Raumsegler1@users.noreply.github.com>
2024-08-25 14:37:14 +0200

    Complete task 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // Convert string to lowercase
+    let lowerstring = string.toLowerCase();
+    
+    // Remove all non-alphanumeric characters (punctuation, spaces, etc.)
+    let string2 = lowerstring.replace(/[^a-z0-9]/g, '');
+    
+    // Reverse the cleaned string
+    let reversedString = string2.split('').reverse().join('');
+    
+    // Compare the cleaned string with its reversed version
+    return string2 === reversedString;
 };
-
+palindromes("3si hannAh is3")
 // Do not edit below this line
 module.exports = palindromes;
