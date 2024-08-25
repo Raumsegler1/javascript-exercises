4c558f2087606ad60a6fe808171c3f54702d96b5
Raumsegler1 <113139832+Raumsegler1@users.noreply.github.com>
2024-08-25 15:04:46 +0200

    Complete task 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num1) {
+num1 = Number(num1);
+if (num1 < 0) return "OOPS";
+fibArr = [0, 1];
+for (let i = 2; i <= num1; i++) {
+    let newFib = fibArr[i-1] + fibArr[i-2];
+    fibArr.push(newFib);
+}
+let result = fibArr[num1]
+return result;
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
