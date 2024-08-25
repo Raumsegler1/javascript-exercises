226b9dc58448d24329e3bae5fa33227223cfcd83
Raumsegler1 <113139832+Raumsegler1@users.noreply.github.com>
2024-08-25 15:32:05 +0200

    Complete task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(arr) {
+    arr.sort((a, b) => {
+        if (a.yearOfDeath && b.yearOfDeath) {return ((a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth))}
+        else if (a.yearOfDeath) {return ((a.yearOfDeath - a.yearOfBirth) - (new Date().getFullYear() - b.yearOfBirth))}
+        else if (b.yearOfDeath) {return ((new Date().getFullYear() - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth))} 
+        else {return ((new Date().getFullYear() - a.yearOfBirth) - (new Date().getFullYear() - b.yearOfBirth))}
+    })
+    let oldest = arr.pop()
+    return oldest 
+};  
 
 // Do not edit below this line
 module.exports = findTheOldest;
